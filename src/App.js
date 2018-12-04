013262dd97a7565dde46bb1dbca262726e0429b5
Ana <anabrmaljkovic@gmail.com>
2018-12-04 16:53:46 -0500

    connected to darksky API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  getWeather = () => {
+    // $.ajax({
+    //   url: 'https://api.darksky.net/forecast/07b4604d016bdc5d5a0f1a893639c687/43.6532,-79.3832,1543924800',
+    //   method: 'GET',
+    //   dataType: 'jsonp'
+    // })
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/07b4604d016bdc5d5a0f1a893639c687/43.6532,-79.3832,1544443200",
+        {
+          method: "GET",
+          contentType: "json"
+        }
+      )
+      .then(res => {
+        console.log(res);
+      });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.getWeather}>Get weather</button>
       </div>
     );
   }
