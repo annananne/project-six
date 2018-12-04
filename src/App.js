657f95fa6424f4eb260f24c49407f8e2db9b640d
Anne <aphits01@gmail.com>
2018-12-04 16:50:46 -0500

    Adding google maps API

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import LocationSearchInput from './LocationSearchInput';
+
+axios
+  .get(
+    "https://cors-anywhere.herokuapp.com/https://www.google.com/maps/embed/v1/directions?key=AIzaSyAwLJ7Hzo0t_a9H7-_2gUN4BbQJ3noHuvA&origin=Oslo+Norway&destination=Telemark+Norway&avoid=tolls|highways",
+    {
+      method: "GET",
+      contentType: "jsonp"
+    }
+  )
+  .then((res) => {
+    // console.log("res", res);
+  });
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      origin: 'Toronto+Ontario',
+      destination: 'Montreal+Quebec'
+    }
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App">
+        <header className="App-header" />
+        <main>
+          <LocationSearchInput />
+          <iframe src={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAwLJ7Hzo0t_a9H7-_2gUN4BbQJ3noHuvA&origin=${this.state.origin}&destination=${this.state.destination}&avoid=tolls|highways`} frameBorder="0" width="600" height="400" title="My map" />
+        </main>
+      </div>;
   }
 }
 
