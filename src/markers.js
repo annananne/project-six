7fc424207209cabcdf7d9c0d29885e839403c15f
Ana <anabrmaljkovic@gmail.com>
2018-12-06 17:55:52 -0500

    added JS map

---
+++ src/markers.js
@@ -0,0 +1,12 @@
+const markers = [
+  {
+    latitude: 43.6532,
+    longitude: -79.3832,
+    isMarkerShown: true
+  },
+  {
+    latitude: 43.589,
+    longitude: -79.6441,
+    isMarkerShown: true
+  }
+];
