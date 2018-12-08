7ffdf24f5f96094d9bd89ce403bd4bee3e6a85a3
Anna Ionova <echoespond@gmail.com>
2018-12-08 13:27:49 -0500

    weather is rendered on the screen in celsius

---
+++ src/components/PointWeatherDisplay.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+// helper function that converts temperature in F to C
+const convertFtoC = (f) => {
+  return Math.ceil((5 / 9) * (f - 32));
+}
+
+class PointWeatherDisplay extends Component {
+  render() {
+    
+    const { 
+      originWeatherData,
+      destinationWeatherData,
+      originAddress,
+      destinationAddress
+    } = this.props;
+
+    const tempOriginC = convertFtoC(originWeatherData.currently.temperature);
+
+    const tempDestinationC = convertFtoC(destinationWeatherData.currently.temperature);
+
+    return (
+      <div>
+    
+        { originWeatherData !== null && 
+        <div>
+          <p>{ originAddress }</p>
+          <p>{ tempOriginC } °C</p>
+        </div>
+        }
+        { destinationWeatherData !== null && 
+        <div>
+        <p>{ destinationAddress }</p>
+          <p>{ tempDestinationC } °C</p>
+        </div>
+        }
+
+      </div>
+    );
+  }
+}
+
+
+export default PointWeatherDisplay;
+
+
+
+
+
